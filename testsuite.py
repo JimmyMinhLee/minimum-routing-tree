50f9c45713f5268717fb5e7db63ac0bdf1fe89e2
Jimmy Lee <46268173+JimmyMinhLee@users.noreply.github.com>
2020-04-21 13:36:36 -0700

    Added dominating set algorithms
    Pulled straight from networkx library; next steps are to figure out how to modify the cost function so that it does better rather than greedily picking - because we're finding the minimum, but we can still add edges that minimize the cost.

--- testsuite.py
+++ testsuite.py
@@ -2,15 +2,16 @@ from userutil import *
 import copy
 
 
-
+# Test graph #
 def create_G():
     G = nx.Graph()
     G.add_edge(1, 2, weight = 5)
     G.add_edge(2, 6, weight = 1)
     G.add_edge(1, 3, weight = 4)
-    G.add_edge(1, 4, weight = 2)
     G.add_edge(3, 4, weight = 1)
+    G.add_edge(1, 4, weight = 1)
     G.add_edge(3, 5, weight = 4)
+    G.to_undirected()
     return G
 
 def test_most_connected():
@@ -25,15 +26,26 @@ def test_get_edge_data():
     edge_data = G.get_edge_data(1, 2)
     print(edge_data)
 
-def test_domset_neighbors():
+def test_vertex_domset():
     T = nx.Graph()
     G = create_G()
-    print("MST cost: {}".format(average_pairwise_distance(get_MST(G))))
-    T = domset_neighbors_alg(G, T)
-    print("T is valid network: {}".format(is_valid_network(create_G(), T)))
-    print("T's cost: {}".format(average_pairwise_distance(T)))
-    print(T.edges())
-    print(T.nodes())
+    mst_g = get_MST(G)
+    print("MST edges: {}".format(mst_g.edges()))
+    print("MST cost: {}".format(average_pairwise_distance(mst_g)))
+    T = domset_approx(G, T)
+    print("T edges: {}".format(T.edges()))
+    print("T cost: {}".format(average_pairwise_distance(T)))
+
+def test_edge_domset():
+    T = nx.Graph()
+    G = create_G()
+    mst_g = get_MST(G)
+    print("MST edges: {}".format(mst_g.edges()))
+    print("MST cost: {}".format(average_pairwise_distance(mst_g)))
+    T = edge_domset_approx(G, T)
+
+
+
 
 
 def test_remove_node():
@@ -50,5 +62,6 @@ def test_remove_node():
 # test_most_connected()
 # test_best_neighbor()
 # test_get_edge_data()
-test_domset_neighbors()
+# test_vertex_domset()
 # test_remove_node()
+test_edge_domset()
--- userutil.py
+++ userutil.py
@@ -1,5 +1,7 @@
 from utils import is_valid_network, average_pairwise_distance
 import networkx as nx
+import grinpy as gp
+import networkx.algorithms.approximation.dominating_set as dom
 import sys, os, random
 
 
@@ -41,65 +43,29 @@ def get_rand():
     input_folder_path + '/medium-' + str(num) + '.in',
     input_folder_path + '/large-' + str(num) + '.in'
 ]
-
 ### SECTION: ALGORITHMS ###
 
-###  First algorithm idea:
-
-# 1. Choose a node with the maximum amount of neighbors.
-# 2. Add it to our tree.
-# 3. Remove this node from the graph.
-# 4. Look for all subsequent vertices that has the maximum amount of neighbors.
-# 5. Do some calculation on the edges; maybe we'd want to make sure that adding
-# this is the best we can do in comparison to all other edges; or even just look
-# at which neighbors have the most vertices.
-# 6. Add this new vertex to the tree.
-# 7. Repeat until the graph is 0.
-
-###
-
 tree = nx.Graph()
-def domset_neighbors_alg(G, T):
-    if len(list(G.nodes())) == 0:
-        return T
-    # 1, 2: get the node with the most amount of neighbors and add it to our tree.
-    best_vertex = get_most_connected_vertex(G)
-    T.add_node(best_vertex)
 
-    # 3: Look for all subsequent vertices that has the maximum amount of neighbors.
-    best_neighbor = get_most_connected_neighbor(G, best_vertex)
-    # Once we have a vertex that has no more numbers, we want to stop the current iteration.
-    if best_neighbor == None:
-        return T
+def domset_approx(G, T):
+    t_set = min_weighted_dominating_set(G)
+    for u in t_set:
+        edges = G.edges(u)
+        for edge in edges:
+            v = edge[1]
+            weight = G.get_edge_data(u, v)['weight']
+            if v in t_set:
+                T.add_edge(u, v, weight=weight)
+    return T
 
-    # 4: Add the corresponding edge between the best vertex and best neighbor.
-    edge = G.get_edge_data(best_vertex, best_neighbor)
-    try:
-        edge_weight = edge['weight']
-    except:
-        print("Caught error at, Vertex: {} Neighbor: {} Edge: {}".format(best_vertex, best_neighbor, edge) )
-        return
-
-    # 5: Add this neighbor and edge to the graph.
-    T.add_edge(best_vertex, best_neighbor, weight=edge_weight)
-
-    # 6: Remove the best vertex from the graph along with all of its neighbors, besides the best one.
-    for neighbor in get_neighbors(G, best_vertex):
-        if neighbor != best_neighbor:
-            G.remove_node(neighbor)
-    G.remove_node(best_vertex)
-
-    # 7: Repeat until the graph has no vertices.
-    domset_neighbors_alg(G, T)
+def edge_domset_approx(G, T):
+    t_set = min_edge_dominating_set(G)
+    for edge in t_set:
+        print("Edge:{}".format(edge))
     return T
 
 
 
-
-
-
-def domset_distance_alg(G):
-    pass
 
 ### SECTION: ALGORITHM UTILITIES ###
 def get_MST(G):
@@ -125,3 +91,116 @@ def get_most_connected_neighbor(G, node):
         if len(list(nx.all_neighbors(G, neighbor))) > len(list(nx.all_neighbors(G, best_neighbor))):
             best_neighbor = neighbor
     return best_neighbor
+
+def get_best_vertex(G):
+    best_vertex = get_most_connected_vertex(G)
+    while len(G.edges(best_vertex)) == 0:
+        if best_vertex == None:
+            return
+        G.remove_node(best_vertex)
+        best_vertex = get_most_connected_vertex(G)
+    return best_vertex
+
+# Ripped from source code #
+def min_weighted_dominating_set(G, weight=None):
+    r"""Returns a dominating set that approximates the minimum weight node
+    dominating set.
+
+    Parameters
+    ----------
+    G : NetworkX graph
+        Undirected graph.
+
+    weight : string
+        The node attribute storing the weight of an node. If provided,
+        the node attribute with this key must be a number for each
+        node. If not provided, each node is assumed to have weight one.
+
+    Returns
+    -------
+    min_weight_dominating_set : set
+        A set of nodes, the sum of whose weights is no more than `(\log
+        w(V)) w(V^*)`, where `w(V)` denotes the sum of the weights of
+        each node in the graph and `w(V^*)` denotes the sum of the
+        weights of each node in the minimum weight dominating set.
+
+    Notes
+    -----
+    This algorithm computes an approximate minimum weighted dominating
+    set for the graph `G`. The returned solution has weight `(\log
+    w(V)) w(V^*)`, where `w(V)` denotes the sum of the weights of each
+    node in the graph and `w(V^*)` denotes the sum of the weights of
+    each node in the minimum weight dominating set for the graph.
+
+    This implementation of the algorithm runs in $O(m)$ time, where $m$
+    is the number of edges in the graph.
+
+    References
+    ----------
+    .. [1] Vazirani, Vijay V.
+           *Approximation Algorithms*.
+           Springer Science & Business Media, 2001.
+
+    """
+    # The unique dominating set for the null graph is the empty set.
+    if len(G) == 0:
+        return set()
+
+    # This is the dominating set that will eventually be returned.
+    dom_set = set()
+
+    def _cost(node_and_neighborhood):
+        """Returns the cost-effectiveness of greedily choosing the given
+        node.
+
+        `node_and_neighborhood` is a two-tuple comprising a node and its
+        closed neighborhood.
+
+        """
+        v, neighborhood = node_and_neighborhood
+        return G.nodes[v].get(weight, 1) / len(neighborhood - dom_set)
+
+    # This is a set of all vertices not already covered by the
+    # dominating set.
+    vertices = set(G)
+    # This is a dictionary mapping each node to the closed neighborhood
+    # of that node.
+    neighborhoods = {v: {v} | set(G[v]) for v in G}
+
+    # Continue until all vertices are adjacent to some node in the
+    # dominating set.
+    while vertices:
+        # Find the most cost-effective node to add, along with its
+        # closed neighborhood.
+        dom_node, min_set = min(neighborhoods.items(), key=_cost)
+        # Add the node to the dominating set and reduce the remaining
+        # set of nodes to cover.
+        dom_set.add(dom_node)
+        del neighborhoods[dom_node]
+        vertices -= min_set
+
+    return dom_set
+
+# Ripped from source code #
+def min_edge_dominating_set(G):
+    r"""Returns minimum cardinality edge dominating set.
+
+    Parameters
+    ----------
+    G : NetworkX graph
+      Undirected graph
+
+    Returns
+    -------
+    min_edge_dominating_set : set
+      Returns a set of dominating edges whose size is no more than 2 * OPT.
+
+    Notes
+    -----
+    The algorithm computes an approximate solution to the edge dominating set
+    problem. The result is no more than 2 * OPT in terms of size of the set.
+    Runtime of the algorithm is $O(|E|)$.
+    """
+    if not G:
+        raise ValueError("Expected non-empty NetworkX graph!")
+    return nx.maximal_matching(G)
